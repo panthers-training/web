8b0311706fc8670e09ca9e4ecea629b92fde1b17
Esteban Victorio <steve@Estebans-Mac-Studio.local>
2026-04-01 12:25:53 -0700

    feat: add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
+import { loadEnv } from "vite";
+const { SITE_URL, PUBLIC_PATH_URL } = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
+  base: PUBLIC_PATH_URL,
   server: {
     port: 3000,
   },
