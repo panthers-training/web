45f924ed72f83fcafccb4701088bd023a11bcf70
Esteban Victorio <steve@Estebans-Mac-Studio.local>
2026-04-01 09:33:31 -0700

    feat: update brief and add about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
